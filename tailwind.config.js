5e98f1c5d0b3ed57b1bff762d27845922f978761
Allan Kirui <allan.kirui@outlook.com>
2023-07-05 22:41:09 +0300

    Add extension to tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "maximum blue green": "#2ad0d0",
+        "night blue": "#3a3054",
+        bittersweet: "#f46262",
+        "x11 gray": "#bfbfbf",
+        manatee: "#9e9aa8",
+        onyx: "#35323e",
+        "raisin black": "#232127",
+      },
+    },
   },
   plugins: [],
 };
